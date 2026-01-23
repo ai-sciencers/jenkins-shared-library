ae45377dcc0b89710bc030fd65f4e4bc5ed452ff
Monika Bharambe <monikabharambe1998@gmail.com>
2026-01-23 12:05:02 +0530

    Implement clone function in clone.groovy
    Add a function to clone a Git repository with a specified branch.

---
+++ vars/clone.groovy
@@ -0,0 +1,6 @@
+def call(string url,string branch){
+  echo "this is cloning the code"
+  git url: 'https://github.com/ai-sciencers/Seo_demo.git', branch: 'master'
+  echo "code clone successfully"
+
+}
